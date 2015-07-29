78d16c1fd1d10437c8eff6e9fcdd94f01a855bd8
Valeri Karpov <valkar207@gmail.com>
2015-07-29 15:34:27 -0400

    step 2; Getting the Currently Logged In User

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -1,4 +1,11 @@
+var loopback = require('loopback');
+
 module.exports = function enableAuthentication(server) {
   // enable authentication
   server.enableAuth();
+
+  server.middleware('auth', loopback.token({
+    model: server.models.accessToken,
+    currentUserLiteral: 'me'
+  }));
 };
